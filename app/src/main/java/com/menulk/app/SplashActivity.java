b1dcc7ad4c6d23b55d7eaa6330ad703b5ce9f0d5
Pavithra <pavidsscst@gmail.com>
2018-01-16 01:53:07 +0530

    Final Commit of Day

--- app/src/main/java/com/menulk/app/SplashActivity.java
+++ app/src/main/java/com/menulk/app/SplashActivity.java
@@ -1,13 +1,37 @@
 package com.menulk.app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.menulk.app.main.ShopActivity;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;  //3 Seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+                                      /*
+                                       * Showing splash screen with a timer. This will be useful when you
+                                       * want to show case your app logo / company
+                                       */
+                                      @Override
+                                      public void run() {
+                                          // This method will be executed once the timer is over
+                                          // Start your app main activity
+                                          Intent i = new Intent(SplashActivity.this, ShopActivity.class);
+                                          startActivity(i);
+
+                                          // close this activity
+                                          finish();
+                                      }
+                                  }, SPLASH_TIME_OUT);
+
+      //  setContentView(R.layout.activity_splash);
     }
 }
