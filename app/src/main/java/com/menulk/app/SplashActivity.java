478b6ebaa283e8320bef9d456628b5c54ece9a4c
Pavithra-Office <pavithra@walkerstours.com>
2018-01-16 12:10:37 +0530

    Splash Screen complete

--- app/src/main/java/com/menulk/app/SplashActivity.java
+++ app/src/main/java/com/menulk/app/SplashActivity.java
@@ -14,6 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         new Handler().postDelayed(new Runnable() {
 
                                       /*
